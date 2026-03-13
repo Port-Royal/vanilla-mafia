1a567c812062b4e09c013b78b9f587c7fd4de650
Denis Kiselev <denis.kiselyov@gmail.com>
2026-03-14 03:03:04 +0700

    203: Add telegram bot API KEY

--- config/initializers/telegram.rb
+++ config/initializers/telegram.rb
@@ -1,2 +1,8 @@
-Rails.application.config.telegram = ActiveSupport::OrderedOptions.new
-Rails.application.config.telegram.bot_token = Rails.application.credentials.dig(:telegram, :bot_token)
+Rails.application.config.x.telegram = ActiveSupport::OrderedOptions.new
+bot_token = Rails.application.credentials.dig(:telegram, :bot_token)
+
+if Rails.env.production? && bot_token.blank?
+  raise "Telegram bot token is missing or blank. Please set credentials.telegram.bot_token."
+end
+
+Rails.application.config.x.telegram.bot_token = bot_token
