83a1de758b05efc6544fadb80eafa0f7632a4565
Denis Kiselev <denis.kiselyov@gmail.com>
2026-03-13 09:47:13 +0700

    255: Update favicon

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,3 +5,15 @@ Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
+
+Rails.application.config.assets.configure do |env|
+  mime_type = 'application/manifest+json'
+  extensions = ['.webmanifest']
+
+  if Sprockets::VERSION.to_i >= 4
+    extensions << '.webmanifest.erb'
+    env.register_preprocessor(mime_type, Sprockets::ERBProcessor)
+  end
+
+  env.register_mime_type(mime_type, extensions:)
+end
