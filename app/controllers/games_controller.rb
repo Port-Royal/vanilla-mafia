7a48600233e06679a0e4607a477540b1c18f4bbf
Denis Kiselev <denis.kiselyov@gmail.com>
2026-03-24 00:26:38 +0700

    201: Implement OBS overlay customization features including font size, color, and visibility options for roles, best move, and seat numbers

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,6 +7,31 @@ class GamesController < ApplicationController
   def overlay
     @game = Game.find(params[:id])
     @participations_by_seat = @game.game_participations.includes(:player, :role).index_by(&:seat)
+    @overlay_config = build_overlay_config
     render layout: "overlay"
+  end
+
+  private
+
+  def build_overlay_config
+    config = {}
+    config[:font_size] = clamp_font_size(params[:font_size])
+    config[:color] = sanitize_hex_color(params[:color])
+    config[:hide_roles] = params[:hide_roles] == "1"
+    config[:hide_best_move] = params[:hide_best_move] == "1"
+    config[:hide_seats] = params[:hide_seats] == "1"
+    config
+  end
+
+  def clamp_font_size(value)
+    return nil unless value.present? && value.match?(/\A\d+\z/)
+
+    value.to_i.clamp(8, 72)
+  end
+
+  def sanitize_hex_color(value)
+    return nil unless value.present? && value.match?(/\A[0-9a-fA-F]{3,8}\z/)
+
+    value.downcase
   end
 end
