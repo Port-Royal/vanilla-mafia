717ae9517f0cd214613541b571e975eff54a1391
Denis Kiselev <denis.kiselyov@gmail.com>
2026-03-24 13:58:45 +0700

    375: Update migration hint and schema version for database changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_24_064338) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_24_065136) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
