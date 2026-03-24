5b9d8327e19f60a9144fccb730f2ab8a18a5b34e
Denis Kiselev <denis.kiselyov@gmail.com>
2026-03-24 17:42:15 +0700

    Revert "171: Make Grant code field editable in Avo"
    This reverts commit d181029c5b283d95fcd33d531ac7802afa755ba9.

--- app/avo/resources/grant.rb
+++ app/avo/resources/grant.rb
@@ -3,7 +3,7 @@ class Avo::Resources::Grant < Avo::BaseResource
 
   def fields
     field :id, as: :id
-    field :code, as: :text
+    field :code, as: :text, readonly: true
     field :user_grants, as: :has_many
   end
 end
